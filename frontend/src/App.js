baf6e3866608d2e91729219cdb4d67f6076f470a
Parkjju <rudwns3927@gmail.com>
2022-07-31 22:42:46 +0900

    feat: RecoilRoot 컴포넌트로 래핑

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import Router from './router';
+import { RecoilRoot } from 'recoil';
+import { Helmet } from 'react-helmet';
 
 function App() {
-  return <Router />;
+    return (
+        <RecoilRoot>
+            <Helmet>
+                <link
+                    rel='stylesheet'
+                    href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0'
+                />
+            </Helmet>
+            <Router />
+        </RecoilRoot>
+    );
 }
 
 export default App;
