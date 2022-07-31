1d26ae02e3801843586a27735ae8073f41db881b
Parkjju <rudwns3927@gmail.com>
2022-07-31 22:43:54 +0900

    feat: Home URL 추가

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/molecules/Login';
 import Register from './components/molecules/Register';
+import Home from './components/pages/Home';
 
 function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default Router;
