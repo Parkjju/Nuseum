96c6fb2d97d5a5930fca32f5b64a1f9a2ef8a57b
Parkjju <rudwns3927@gmail.com>
2022-08-01 17:57:14 +0900

    fix: 컨텐츠 배열 CSS 수정

--- frontend/src/components/pages/Home/index.jsx
+++ frontend/src/components/pages/Home/index.jsx
@@ -1,13 +1,25 @@
 import Title from '../../atom/Title';
-import { Contents, Logo } from './styled';
-import logo from '../../../assets/logo.jpeg';
+import { Contents } from './styled';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { token } from '../../../recoil/token/token';
+import Card from '../../atom/Card';
 
 function Home() {
+    const navigate = useNavigate();
+    const tokenValue = useRecoilValue(token);
+    useEffect(() => {
+        if (!tokenValue) {
+            navigate('/login');
+            return;
+        }
+    }, [tokenValue, navigate]);
     return (
         <>
-            <Title text='Home' />;
+            <Title text='HOME' />
             <Contents>
-                <Logo src={logo} />
+                <Card />
             </Contents>
         </>
     );
