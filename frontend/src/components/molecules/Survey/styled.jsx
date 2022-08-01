bbbe4bfe71e6b38a753a920158d55de7d593b1fe
Parkjju <rudwns3927@gmail.com>
2022-08-01 17:57:45 +0900

    style: 설문조사 각 문항 컴포넌트화 및 퍼블리싱

---
+++ frontend/src/components/molecules/Survey/styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const QuestionBox = styled.div`
+    height: 400px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const QuestionHeader = styled.p`
+    height: 50px;
+    font-size: 16px;
+    width: 300px;
+    color: black;
+`;
+export const QuestionBody = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
