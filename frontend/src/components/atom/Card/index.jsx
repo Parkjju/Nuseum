062e3dedacb2793743c59762842e42826b4ac036
Parkjju <rudwns3927@gmail.com>
2022-08-03 15:20:49 +0900

    fix: 카드 Container 컴포넌트로 감싼 뒤 중앙정렬

--- frontend/src/components/atom/Card/index.jsx
+++ frontend/src/components/atom/Card/index.jsx
@@ -1,64 +1,69 @@
 import { Box, Icon, Name, Tab } from './styled';
 import { Link, useParams } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import Container from '../Container';
 
 function Card({ menu, current }) {
     const params = useParams();
 
     return (
-        <Box>
-            {menu.map((item, index) => (
-                <Link
-                    key={index}
-                    to={
-                        params.date ? `${params.date}/${item[2]}` : `${item[2]}`
-                    }
-                >
-                    <AnimatePresence>
-                        {current === 'home' ? (
-                            <Tab layoutId={item[2]}>
-                                {item[1] === '저녁' ? (
-                                    <>
-                                        <Icon
-                                            style={{ width: '50px' }}
-                                            src={item[0]}
-                                        />
-                                        <Name>{item[1]}</Name>
-                                    </>
-                                ) : (
-                                    <>
-                                        <Icon src={item[0]} />
-                                        <Name>{item[1]}</Name>
-                                    </>
-                                )}
-                            </Tab>
-                        ) : (
-                            <Tab
-                                layoutId={item[2]}
-                                transition={{
-                                    velocity: 10,
-                                }}
-                            >
-                                {item[1] === '저녁' ? (
-                                    <>
-                                        <Icon
-                                            style={{ width: '50px' }}
-                                            src={item[0]}
-                                        />
-                                        <Name>{item[1]}</Name>
-                                    </>
-                                ) : (
-                                    <>
-                                        <Icon src={item[0]} />
-                                        <Name>{item[1]}</Name>
-                                    </>
-                                )}
-                            </Tab>
-                        )}
-                    </AnimatePresence>
-                </Link>
-            ))}
-        </Box>
+        <Container>
+            <Box>
+                {menu.map((item, index) => (
+                    <Link
+                        key={index}
+                        to={
+                            params.date
+                                ? `${params.date}/${item[2]}`
+                                : `${item[2]}`
+                        }
+                    >
+                        <AnimatePresence>
+                            {current === 'home' ? (
+                                <Tab layoutId={item[2]}>
+                                    {item[1] === '저녁' ? (
+                                        <>
+                                            <Icon
+                                                style={{ width: '50px' }}
+                                                src={item[0]}
+                                            />
+                                            <Name>{item[1]}</Name>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <Icon src={item[0]} />
+                                            <Name>{item[1]}</Name>
+                                        </>
+                                    )}
+                                </Tab>
+                            ) : (
+                                <Tab
+                                    layoutId={item[2]}
+                                    transition={{
+                                        velocity: 10,
+                                    }}
+                                >
+                                    {item[1] === '저녁' ? (
+                                        <>
+                                            <Icon
+                                                style={{ width: '50px' }}
+                                                src={item[0]}
+                                            />
+                                            <Name>{item[1]}</Name>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <Icon src={item[0]} />
+                                            <Name>{item[1]}</Name>
+                                        </>
+                                    )}
+                                </Tab>
+                            )}
+                        </AnimatePresence>
+                    </Link>
+                ))}
+            </Box>
+        </Container>
     );
 }
 export default Card;
