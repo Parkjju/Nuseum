c29c4ba82443aeee33ce343d216c4b70010f00fa
Parkjju <rudwns3927@gmail.com>
2022-08-04 15:06:21 +0900

    style: 컨테이너 레이아웃 min-height 설정 및 카드 컴포넌트 Container Wrapper 제거

--- frontend/src/components/atom/Card/index.jsx
+++ frontend/src/components/atom/Card/index.jsx
@@ -7,63 +7,59 @@ function Card({ menu, current }) {
     const params = useParams();
 
     return (
-        <Container>
-            <Box>
-                {menu.map((item, index) => (
-                    <Link
-                        key={index}
-                        to={
-                            params.date
-                                ? `${params.date}/${item[2]}`
-                                : `${item[2]}`
-                        }
-                    >
-                        <AnimatePresence>
-                            {current === 'home' ? (
-                                <Tab layoutId={item[2]}>
-                                    {item[1] === '저녁' ? (
-                                        <>
-                                            <Icon
-                                                style={{ width: '50px' }}
-                                                src={item[0]}
-                                            />
-                                            <Name>{item[1]}</Name>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <Icon src={item[0]} />
-                                            <Name>{item[1]}</Name>
-                                        </>
-                                    )}
-                                </Tab>
-                            ) : (
-                                <Tab
-                                    layoutId={item[2]}
-                                    transition={{
-                                        velocity: 10,
-                                    }}
-                                >
-                                    {item[1] === '저녁' ? (
-                                        <>
-                                            <Icon
-                                                style={{ width: '50px' }}
-                                                src={item[0]}
-                                            />
-                                            <Name>{item[1]}</Name>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <Icon src={item[0]} />
-                                            <Name>{item[1]}</Name>
-                                        </>
-                                    )}
-                                </Tab>
-                            )}
-                        </AnimatePresence>
-                    </Link>
-                ))}
-            </Box>
-        </Container>
+        <Box>
+            {menu.map((item, index) => (
+                <Link
+                    key={index}
+                    to={
+                        params.date ? `${params.date}/${item[2]}` : `${item[2]}`
+                    }
+                >
+                    <AnimatePresence>
+                        {current === 'home' ? (
+                            <Tab layoutId={item[2]}>
+                                {item[1] === '저녁' ? (
+                                    <>
+                                        <Icon
+                                            style={{ width: '50px' }}
+                                            src={item[0]}
+                                        />
+                                        <Name>{item[1]}</Name>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Icon src={item[0]} />
+                                        <Name>{item[1]}</Name>
+                                    </>
+                                )}
+                            </Tab>
+                        ) : (
+                            <Tab
+                                layoutId={item[2]}
+                                transition={{
+                                    velocity: 10,
+                                }}
+                            >
+                                {item[1] === '저녁' ? (
+                                    <>
+                                        <Icon
+                                            style={{ width: '50px' }}
+                                            src={item[0]}
+                                        />
+                                        <Name>{item[1]}</Name>
+                                    </>
+                                ) : (
+                                    <>
+                                        <Icon src={item[0]} />
+                                        <Name>{item[1]}</Name>
+                                    </>
+                                )}
+                            </Tab>
+                        )}
+                    </AnimatePresence>
+                </Link>
+            ))}
+        </Box>
     );
 }
 export default Card;
