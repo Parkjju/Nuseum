f3054b8e3edeb7a84a94d1d897ba3ed457177303
Parkjju <rudwns3927@gmail.com>
2022-08-11 21:25:01 +0900

    feature: 전역상태 selector 전체 삭제 및 setter 로직 활용

--- frontend/src/recoil/period/period.js
+++ frontend/src/recoil/period/period.js
@@ -1,40 +1,11 @@
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 
 export const periodState = atom({
     key: 'period',
     default: {
-        breakfast: undefined,
-        lunch: undefined,
-        dinner: undefined,
-        snack: undefined,
-    },
-});
-export const breakFast = selector({
-    key: 'derived',
-    get: ({ get }) => {
-        const originStateArray = get(periodState);
-        return { ...originStateArray.breakfast };
-    },
-});
-
-export const lunch = selector({
-    key: 'derived',
-    get: ({ get }) => {
-        const originStateArray = get(periodState);
-        return { ...originStateArray.lunch };
-    },
-});
-export const dinner = selector({
-    key: 'derived',
-    get: ({ get }) => {
-        const originStateArray = get(periodState);
-        return { ...originStateArray.dinner };
-    },
-});
-export const snack = selector({
-    key: 'derived',
-    get: ({ get }) => {
-        const originStateArray = get(periodState);
-        return { ...originStateArray.snack };
+        breakfast: [],
+        lunch: [],
+        dinner: [],
+        snack: [],
     },
 });
