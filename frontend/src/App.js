94588fa7917a1f963f073690276ed2e3f7114253
Parkjju <rudwns3927@gmail.com>
2022-08-12 10:16:26 +0900

    feature: 새로고침 방지 기능 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,6 +74,12 @@ body{
 `;
 
 function App() {
+    window.addEventListener('beforeunload', function (e) {
+        // Cancel the event
+        e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+        // Chrome requires returnValue to be set
+        e.returnValue = '';
+    });
     return (
         <RecoilRoot>
             <Helmet>
