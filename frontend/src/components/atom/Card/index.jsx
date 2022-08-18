c296034b6f24f4c20dd7c6ba4fff6856d675c61f
Parkjju <rudwns3927@gmail.com>
2022-08-18 17:12:14 +0900

    fix: console.log 삭제 , isDateClicked 전역상태 삭제

--- frontend/src/components/atom/Card/index.jsx
+++ frontend/src/components/atom/Card/index.jsx
@@ -4,7 +4,6 @@ import { AnimatePresence } from 'framer-motion';
 
 function Card({ menu, current }) {
     const params = useParams();
-    console.log(params);
 
     return (
         <Box>
--- frontend/src/recoil/calendarClicked/calendarClicked.js
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const calendarClicked = atom({
-    key: 'calendarClicked',
-    default: false,
-});
