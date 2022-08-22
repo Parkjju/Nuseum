8358fe9027eba3c9f471766ed63bab87dba274a4
Parkjju <rudwns3927@gmail.com>
2022-08-22 13:44:32 +0900

    feat: POST / PUT API 수정 및 데이터 저장 로직 개선, period state 관리

--- frontend/src/components/pages/Diary/index.jsx
+++ frontend/src/components/pages/Diary/index.jsx
@@ -11,7 +11,7 @@ import Card from '../../atom/Card';
 import { Contents } from '../Home/styled';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { constSelector, useRecoilState, useRecoilValue } from 'recoil';
 import { mealImageState, periodState } from '../../../recoil/period/period';
 import axios from 'axios';
 import { FormBox } from '../../molecules/Login/styled';
@@ -25,6 +25,7 @@ function Diary({ date }) {
             navigate('/login');
         }
     }, []);
+
     const menu = [
         [morning, '아침', 'breakfast'],
         [lunch, '점심', 'lunch'],
@@ -36,55 +37,18 @@ function Diary({ date }) {
     ];
     const meal = useRecoilValue(periodState);
     const mealImages = useRecoilValue(mealImageState);
+
     const [postId, setPostId] = useRecoilState(postIdState);
 
-    const makePutRequestData = (meal) => {
-        let postData = { ...meal };
-        console.log('PUTREQUESTDATA function', postData);
-        for (let i of Object.keys(postData)) {
-            let amountSaveArray = [];
-            if (postData[i].length === 0) {
-                postData = {
-                    ...postData,
-                    [`${i}`]: [],
-                    [`${i}_amount`]: JSON.stringify([]),
-                };
-                continue;
-            }
-            postData[i] = postData[i].map((amount) => {
-                amountSaveArray.push(amount.at(-1));
-                let copy = [...amount];
-                copy.pop();
-                return [...copy];
-            });
-
-            postData = {
-                ...postData,
-                [`${i}`]: [...postData[i].flat()],
-                [`${i}_amount`]: JSON.stringify([...amountSaveArray]),
-            };
+    const makeRequestData = (data, date) => {
+        let result = { meal: [], created_at: `${date.getTime()}` };
+        for (let i in data) {
+            result.meal.push(data[i]);
         }
+        result.meal = result.meal.flat();
+        console.log('FLAT', result);
 
-        return { ...postData };
-    };
-
-    const makePostRequestData = (meal) => {
-        let postData = { ...meal };
-        for (let i in postData) {
-            if (postData[i] !== []) {
-                postData[i] = postData[i].map((item) => {
-                    if (item.length === 2) {
-                        return;
-                    }
-                    let copyItem = [...item];
-                    copyItem.shift();
-                    copyItem = copyItem.map((element) => Number(element));
-                    return [...copyItem];
-                });
-            }
-        }
-
-        return { ...postData };
+        return { ...result };
     };
 
     const deleteFoodName = (postData) => {
@@ -97,39 +61,29 @@ function Diary({ date }) {
                 let copy = [...meal];
                 copy.shift();
                 copy = copy.map((item) => Number(item));
+                copy.push(i);
                 return [...copy];
             });
         }
         return { ...postData };
     };
 
-    console.log(postId.id);
     const onSubmit = (e) => {
         e.preventDefault();
         let postData = { ...meal };
 
         postData = { ...deleteFoodName({ ...postData }) };
 
-        if (postId.id === undefined || postId.id === null) {
-            let temp = makePostRequestData({ ...meal });
-            postData = { ...temp };
-        } else {
-            let temp = makePutRequestData({ ...postData });
-            postData = { ...temp };
-        }
-        if (postId.id === null || postId.id === undefined) {
-            console.log('POST직전');
-            console.log('POST 직전 정제 데이터', postData);
+        let requestData = makeRequestData(postData, date);
+        console.log(requestData);
+
+        if (postId === null || postId === undefined) {
             axios
                 .post(
-                    'https://cryptic-castle-40575.herokuapp.com/api/v1/post/',
+                    'https://nuseum-server.herokuapp.com/api/v1/post/',
                     {
-                        breakfast: [...postData.breakfast],
-                        lunch: [...postData.lunch],
-                        dinner: [...postData.dinner],
-                        snack: [...postData.snack],
-                        supplement: [...postData.supplement],
-                        created_at: `${date.getTime()}`,
+                        meal: [...requestData.meal],
+                        created_at: requestData.created_at,
                     },
                     {
                         headers: {
@@ -140,54 +94,20 @@ function Diary({ date }) {
                     }
                 )
                 .then((response) => {
-                    console.log('일지 등록이 완료되었어요☺️');
+                    alert('일지 등록이 완료되었어요☺️');
                     setPostId(() => {
                         return {
                             id: response.data.id,
                         };
                     });
                 })
-                .catch((err) => console.log(err));
+                .catch((err) => console.log('POST Error'));
         } else {
-            console.log('PUT 날리는중..');
-            console.log('PUT 최종 데이터:', {
-                breakfast: [...postData.breakfast],
-                breakfast_amount: postData.breakfast_amount,
-                lunch: [...postData.lunch],
-                lunch_amount: postData.lunch_amount,
-                dinner_amount: postData.dinner_amount,
-                dinner: [...postData.dinner],
-                snack: [...postData.snack],
-                snack_amount: postData.snack_amount,
-                supplement: [...postData.supplement],
-                supplement_amount: postData.supplement_amount,
-            });
             axios
                 .put(
-                    `https://cryptic-castle-40575.herokuapp.com/api/v1/post/${postId.id}/`,
+                    `https://nuseum-server.herokuapp.com/api/v1/post/${postId}/`,
                     {
-                        breakfast: [...postData.breakfast],
-                        breakfast_amount: postData.breakfast_amount,
-                        lunch: [...postData.lunch],
-                        lunch_amount: postData.lunch_amount,
-                        dinner_amount: postData.dinner_amount,
-                        dinner: [...postData.dinner],
-                        snack: [...postData.snack],
-                        snack_amount: postData.snack_amount,
-                        supplement: [...postData.supplement],
-                        supplement_amount: postData.supplement_amount,
-                        // breakfast_img1: mealImages.breakfast_img1,
-                        // breakfast_img2: mealImages.breakfast_img2,
-                        // breakfast_img3: mealImages.breakfast_img3,
-                        // lunch_img1: mealImages.lunch_img1,
-                        // lunch_img2: mealImages.lunch_img2,
-                        // lunch_img3: mealImages.lunch_img3,
-                        // dinner_img1: mealImages.dinner_img1,
-                        // dinner_img2: mealImages.dinner_img2,
-                        // dinner_img3: mealImages.dinner_img3,
-                        // snack_img1: mealImages.snack1,
-                        // snack_img2: mealImages.snack2,
-                        // snack_img3: mealImages.snack3,
+                        meal: [...requestData.meal],
                     },
                     {
                         headers: {
@@ -198,7 +118,7 @@ function Diary({ date }) {
                     }
                 )
                 .then((response) => alert('일지 수정이 완료되었어요☺️'))
-                .catch((err) => console.log(err));
+                .catch((err) => console.log('put Error'));
         }
     };
 
