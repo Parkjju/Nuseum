b1ffc0f5391fb4d42605343cf80fa0b21f57a5d2
Parkjju <rudwns3927@gmail.com>
2022-09-01 14:07:15 +0900

    feat: pwa install event listening

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -83,6 +83,20 @@ function App() {
         // Chrome requires returnValue to be set
         e.returnValue = '';
     });
+
+    // Initialize deferredPrompt for use later to show browser install prompt.
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent the mini-infobar from appearing on mobile
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        // Update UI notify the user they can install the PWA
+        alert('HELLO PWA!');
+        // Optionally, send analytics event that PWA install promo was shown.
+        console.log(`'beforeinstallprompt' event was fired.`);
+    });
     return (
         <RecoilRoot>
             <Helmet>
