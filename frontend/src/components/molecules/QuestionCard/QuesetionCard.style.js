339e85f362e5502d4934d0efd33c7403a5c209ea
Parkjju <rudwns3927@gmail.com>
2022-09-14 17:19:24 +0900

    feat: QuestionCard 컴포넌트 제작 / 답변완료 등 API 연동 데이터 받아온 후 렌더링

--- frontend/src/components/molecules/QuestionCard/QuesetionCard.style.js
+++ frontend/src/components/molecules/QuestionCard/QuesetionCard.style.js
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const Box = styled.div`
+export const Box = styled(Link)`
     height: 50px;
     width: 80%;
     border-bottom: 1px solid rgba(127, 140, 141, 0.5);
@@ -12,4 +13,6 @@ export const Box = styled.div`
         opacity: 0.6;
         transition: 0.2s linear;
     }
+    text-decoration: none;
+    color: black;
 `;
--- frontend/src/components/molecules/QuestionCard/QuestionCard.jsx
+++ frontend/src/components/molecules/QuestionCard/QuestionCard.jsx
@@ -1,12 +1,24 @@
+import { Link } from 'react-router-dom';
 import { Box } from './QuesetionCard.style';
 
-const QuestionCard = ({ data, isAnswered }) => {
+const QuestionCard = ({ data, isAnswered, id }) => {
+    const currentWidth = window.innerWidth;
     return (
-        <Box>
+        <Box to={`./${id}`}>
             <p>
                 <span style={{ fontSize: 16, marginRight: 5 }}>Q.</span>
                 <span style={{ fontSize: 14, fontWeight: 100 }}>
-                    {data.length > 20 ? `${data.slice(0, 20)}...` : data}
+                    {currentWidth < 300
+                        ? data.length > 10
+                            ? `${data.slice(0, 10)}...`
+                            : data
+                        : currentWidth < 480
+                        ? data.length > 15
+                            ? `${data.slice(0, 15)}...`
+                            : data
+                        : data.length > 25
+                        ? `${data.slice(0, 25)}...`
+                        : data}
                 </span>
             </p>
 
