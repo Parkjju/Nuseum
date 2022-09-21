ceb048a3454841a1b396ab8005eb50cd85c813e3
Parkjju <rudwns3927@gmail.com>
2022-09-21 16:48:10 +0900

    fix: package.json proxy삭제 / setupProxy.js 복구 및 api heroku server 연결

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/api', // 불러오려는 server 의 api path
+        createProxyMiddleware({
+            target: 'https://cryptic-castle-40575.herokuapp.com',
+            // process.env.NODE_ENV === 'development'
+            //     ? 'http://localhost:8000'
+            //     : 'https://cryptic-castle-40575.herokuapp.com',
+            changeOrigin: true,
+        })
+    );
+};
