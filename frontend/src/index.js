5019440a9e2718356a48ba33ee520140bc0b4b08
Parkjju <rudwns3927@gmail.com>
2022-09-21 16:02:33 +0900

    feat: axios baseURL production 분기 후 api서버로 baseURL등록 / process.env.NODE_ENV 코드 출력

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,17 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
+// axios.defaults.baseURL = 'http://localhost:8000';
+// axios.defaults.baseURL = 'https://cryptic-castle-40575.herokuapp.com';
+axios.defaults.baseURL =
+    process.env.NODE_ENV === 'development'
+        ? '/'
+        : 'https://cryptic-castle-40575.herokuapp.com';
+
+console.log(process.env.NODE_ENV);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
