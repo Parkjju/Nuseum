69118487f6a314e39a0c9409b50bb212bb559500
Parkjju <rudwns3927@gmail.com>
2022-09-21 16:37:35 +0900

    fix: baseURL 주석처리

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,10 +9,10 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 // axios.defaults.baseURL = 'http://localhost:8000';
 // axios.defaults.baseURL = 'https://cryptic-castle-40575.herokuapp.com';
-axios.defaults.baseURL =
-    process.env.NODE_ENV === 'development'
-        ? '/'
-        : 'https://cryptic-castle-40575.herokuapp.com';
+// axios.defaults.baseURL =
+//     process.env.NODE_ENV === 'development'
+//         ? 'http://localhost:8000'
+//         : 'https://cryptic-castle-40575.herokuapp.com';
 
 console.log(process.env.NODE_ENV);
 
@@ -27,3 +27,18 @@ root.render(
 
 serviceWorkerRegistration.register();
 reportWebVitals();
+
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = function (app) {
+//     app.use(
+//         '/api', // 불러오려는 server 의 api path
+//         createProxyMiddleware({
+//             target: 'https://cryptic-castle-40575.herokuapp.com',
+//             // process.env.NODE_ENV === 'development'
+//             //     ? 'http://localhost:8000'
+//             //     : 'https://cryptic-castle-40575.herokuapp.com',
+//             changeOrigin: true,
+//         })
+//     );
+// };
