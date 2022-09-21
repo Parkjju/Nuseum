dacce887f1d1fb0dbb54d237383b9865330eea53
Parkjju <rudwns3927@gmail.com>
2022-09-21 16:01:50 +0900

    feat: setupProxy 주석처리

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/api', // 불러오려는 server 의 api path
+        '/proxy', // 불러오려는 server 의 api path
         createProxyMiddleware({
             target: 'https://cryptic-castle-40575.herokuapp.com', // server 주소를 넣어주면 된다.
             changeOrigin: true,
