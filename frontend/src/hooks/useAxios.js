a9abcbe078e01c3565e9056557e42d0ee1634c01
Parkjju <rudwns3927@gmail.com>
2022-09-24 23:23:47 +0900

    feat: useAxios custom hook 추가

---
+++ frontend/src/hooks/useAxios.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+const useAxios = (requestConfig, applyData) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const sendRequest = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios({
+                method: requestConfig.method,
+                url: requestConfig.url,
+                data: {
+                    ...applyData,
+                },
+                headers: requestConfig.headers,
+            });
+
+            console.log(response.data);
+        } catch (error) {
+            alert('에러가 발생하였습니다. 개발자에게 문의해주세요!');
+        }
+        setIsLoading(false);
+    };
+
+    return {
+        isLoading,
+        sendRequest,
+    };
+};
+
+export default useAxios;
