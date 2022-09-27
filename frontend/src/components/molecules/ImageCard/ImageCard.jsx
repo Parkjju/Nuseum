e0b24df62ff59f631df239ddaf7a365bcae8ba16
Parkjju <rudwns3927@gmail.com>
2022-09-27 23:16:23 +0900

    feat: 카드 컴포넌트 로직 변경 - post전용 데이터를 dispatch로 관리 / 서버 통신 후 비활성화 / useEffect로 dispatch 비우고 객체로 새로고침

--- frontend/src/components/molecules/ImageCard/ImageCard.jsx
+++ frontend/src/components/molecules/ImageCard/ImageCard.jsx
@@ -10,14 +10,21 @@ import imageCompression from 'browser-image-compression';
 import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { supplementState } from '../../../recoil/supplement/supplement';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import useActions from '../../../hooks/useActions';
+import { supplementActions } from '../../../store/supplement-slice';
+import axios from 'axios';
+import { CircularProgress } from '@mui/material';
 
-const ImageCard = ({ index, data }) => {
+const ImageCard = ({ isSaved, index, data, setFetchedSupplement }) => {
+    // index에 접근하여 해당 데이터 수정
+
     const params = useParams();
     const action = useActions(params.when);
+    const supplementData = useSelector((state) => state.supplement.data);
     const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
 
     const [currentImage, setCurrentImage] = useState(data ? data.image : null);
     const [supplementName, setSupplementName] = useState(data ? data.name : '');
@@ -58,7 +65,13 @@ const ImageCard = ({ index, data }) => {
             reader.readAsDataURL(compressedFile);
             reader.onloadend = () => {
                 const base64data = reader.result;
-                setCurrentImage(base64data);
+                dispatch(
+                    supplementActions.modifyData({
+                        type: 'image',
+                        index,
+                        data: base64data,
+                    })
+                );
             };
         } catch (error) {
             console.log(error);
@@ -67,12 +80,28 @@ const ImageCard = ({ index, data }) => {
 
     const onChangeManufacture = (e) => {
         setManufacturer(e.target.value);
+        dispatch(
+            supplementActions.modifyData({
+                data: e.target.value,
+                index,
+                type: 'manufacturer',
+            })
+        );
     };
     const onChangeSupplementName = (e) => {
         setSupplementName(e.target.value);
+        dispatch(
+            supplementActions.modifyData({
+                data: e.target.value,
+                index,
+                type: 'name',
+            })
+        );
     };
 
-    return (
+    return loading ? (
+        <CircularProgress sx={{ margin: '100px auto' }} />
+    ) : (
         <Container>
             <ImageBox>
                 {data && data.image ? (
@@ -105,19 +134,53 @@ const ImageCard = ({ index, data }) => {
                     value={manufacturer}
                     onChange={onChangeManufacture}
                     placeholder='제조사'
-                    disabled={isUsableData && !isNew}
+                    disabled={(isUsableData && !isNew) || isSaved}
                 />
                 <DescriptionInput
                     value={supplementName}
                     placeholder='영양제 이름'
                     onChange={onChangeSupplementName}
-                    disabled={isUsableData && !isNew}
+                    disabled={(isUsableData && !isNew) || isSaved}
                 />
             </DescriptionBox>
             <span
                 style={{ cursor: 'pointer' }}
                 className='material-symbols-outlined'
-                onClick={() => dispatch(action.removeImage())}
+                onClick={async () => {
+                    if (window.confirm('입력한 영양제 정보를 삭제할까요?')) {
+                        setLoading(true);
+                        try {
+                            if (data.id) {
+                                await axios.delete(
+                                    `https://nuseum-v2.herokuapp.com/api/v1/consumption/supplement/${data.id}/`
+                                );
+
+                                // fetched로 불러온거 삭제하는 로직
+                                setFetchedSupplement((prev) => {
+                                    let count = 0;
+                                    for (let obj of prev) {
+                                        if (obj.id === data.id) {
+                                            return [
+                                                ...prev.slice(0, count),
+                                                ...prev.slice(count + 1),
+                                            ];
+                                        }
+                                    }
+                                });
+                            } else {
+                                dispatch(action.removeData(index));
+                            }
+                            setLoading(false);
+                            alert('영양제 정보가 삭제되었습니다!');
+                        } catch (err) {
+                            console.log(err);
+                            alert(
+                                '오류가 발생했습니다. 담당자에게 문의해주세요!'
+                            );
+                            setLoading(false);
+                        }
+                    }
+                }}
             >
                 delete
             </span>
