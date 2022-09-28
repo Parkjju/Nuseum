307c1296141a7b767eee690a0c264043c9e7c7e7
Parkjju <rudwns3927@gmail.com>
2022-09-28 22:21:52 +0900

    fix: 주석 삭제 및 App컴포넌트 Proider로 래핑

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,39 +5,20 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
 import axios from 'axios';
+import store from './store/store';
+import { Provider } from 'react-redux';
 
 axios.defaults.withCredentials = true;
-// axios.defaults.baseURL = 'https://cryptic-castle-40575.herokuapp.com';
-// axios.defaults.baseURL =
-//     process.env.NODE_ENV === 'development'
-//         ? 'http://localhost:8000'
-//         : 'https://cryptic-castle-40575.herokuapp.com';
-
-console.log(process.env.NODE_ENV);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <RecoilRoot>
-            <App />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </RecoilRoot>
     </React.StrictMode>
 );
 
 serviceWorkerRegistration.register();
 reportWebVitals();
-
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// module.exports = function (app) {
-//     app.use(
-//         '/api', // 불러오려는 server 의 api path
-//         createProxyMiddleware({
-//             target: 'https://cryptic-castle-40575.herokuapp.com',
-//             // process.env.NODE_ENV === 'development'
-//             //     ? 'http://localhost:8000'
-//             //     : 'https://cryptic-castle-40575.herokuapp.com',
-//             changeOrigin: true,
-//         })
-//     );
-// };
