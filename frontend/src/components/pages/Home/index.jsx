69f026e9768401bea676f01014289ce2907be3dc
Parkjju <rudwns3927@gmail.com>
2022-09-29 01:44:12 +0900

    feat: 홈화면 초기 렌더링 시 token refresh API로 요청 보내보고 문제 발생하면 로그인 페이지로 리다이렉트

--- frontend/src/components/pages/Home/index.jsx
+++ frontend/src/components/pages/Home/index.jsx
@@ -9,8 +9,64 @@ import food from '../../../assets/food.png';
 import question from '../../../assets/q&a.png';
 import { DiaryTitle } from '../Record/styled';
 import { Name } from '../../atom/Card/styled';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { authActions } from '../../../store/auth-slice';
+
+let init = true;
 
 function Home() {
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (init) {
+            init = false;
+            return;
+        } else {
+            if (isLoggedIn) {
+                return;
+            }
+            axios
+                .post(
+                    'https://nuseum-v2.herokuapp.com/api/v1/account/token/refresh/',
+                    {},
+                    {
+                        headers: {
+                            Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxLCJpYXQiOjEsImp0aSI6ImFjZTcxMzE5YmVkMDQwYzFhMWMxODgyNGYzOWUzNTVlIiwidXNlcl9pZCI6MH0.P1e_v6fDHgG4qaODzLDvKTFgGBBNK7pmH_9M--MpfwA`,
+                        },
+                    }
+                )
+                .then((response) => {
+                    console.log('response: ', response.data);
+                    const decodedData = jwt_decode(response.data.access);
+                    dispatch(
+                        authActions.login({
+                            token: response.data.access,
+                            expiration_time: decodedData.exp,
+                        })
+                    );
+                })
+                .catch((err) => {
+                    if (err.response.data.err_code === 'NOT_ACCEPTABLE') {
+                        dispatch(authActions.logout());
+                        navigate('/login');
+                    }
+                    if (err.response.data.detail === 'Token is blacklisted') {
+                        dispatch(authActions.logout());
+                        alert(
+                            '다른 곳에서 해당 계정으로 로그인하였습니다. 다시 로그인해주세요!'
+                        );
+                        navigate('/login');
+                    }
+                });
+            init = true;
+        }
+    }, [dispatch]);
+
     const menu = [
         [diary, '식단일기', 'diary'],
         [analysis, '식이분석', 'analysis'],
