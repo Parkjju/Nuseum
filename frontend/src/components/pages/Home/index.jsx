1e051e98efc0270cc9848801a9aacfe2ee25de46
Parkjju <rudwns3927@gmail.com>
2022-09-29 14:40:21 +0900

    feat: 홈페이지 리프레시토큰 만료 로직 추가

--- frontend/src/components/pages/Home/index.jsx
+++ frontend/src/components/pages/Home/index.jsx
@@ -51,6 +51,12 @@ function Home() {
                     );
                 })
                 .catch((err) => {
+                    // 리프레시토큰 만료
+                    if (err.response.data.code === 'token_not_valid') {
+                        alert('세션이 만료되었습니다. 다시 로그인해주세요!');
+                        dispatch(authActions.logout());
+                        navigate('/login');
+                    }
                     if (err.response.data.err_code === 'NOT_ACCEPTABLE') {
                         dispatch(authActions.logout());
                         navigate('/login');
