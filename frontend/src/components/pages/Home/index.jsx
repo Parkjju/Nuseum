5ae68e47678543de1140c2e99f7d16f2295e5ee7
Parkjju <rudwns3927@gmail.com>
2022-10-04 10:33:35 +0900

    feat: silentRefresh 기능 추가

--- frontend/src/components/pages/Home/index.jsx
+++ frontend/src/components/pages/Home/index.jsx
@@ -11,10 +11,10 @@ import { DiaryTitle } from '../Record/styled';
 import { Name } from '../../atom/Card/styled';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import jwt_decode from 'jwt-decode';
+import { useDispatch } from 'react-redux';
+
 import { authActions } from '../../../store/auth-slice';
+import handleExpired from '../../../helpers/handleExpired';
 
 let init = true;
 
@@ -26,57 +26,18 @@ function Home() {
         if (!isLoggedIn) {
             navigate('/login');
         }
-        if (init) {
-            init = false;
-            return;
-        } else {
-            if (isLoggedIn) {
-                return;
-            }
-            axios
-                .post(
-                    'https://nuseum-v2.herokuapp.com/api/v1/account/token/refresh/',
-                    {},
-                    {
-                        headers: {
-                            Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxLCJpYXQiOjEsImp0aSI6ImFjZTcxMzE5YmVkMDQwYzFhMWMxODgyNGYzOWUzNTVlIiwidXNlcl9pZCI6MH0.P1e_v6fDHgG4qaODzLDvKTFgGBBNK7pmH_9M--MpfwA`,
-                        },
-                    }
-                )
-                .then((response) => {
-                    const decodedData = jwt_decode(response.data.access);
-                    dispatch(
-                        authActions.login({
-                            token: response.data.access,
-                            expiration_time: decodedData.exp,
-                        })
-                    );
-                })
-                .catch((err) => {
-                    // 리프레시토큰 만료
-                    if (
-                        err.response.data.messages[0].token_type === 'refresh'
-                    ) {
-                        alert('세션이 만료되었습니다. 다시 로그인해주세요!');
-                        dispatch(authActions.logout());
-                        navigate('/login');
-                    }
-                    if (err.response.data.err_code === 'NOT_ACCEPTABLE') {
-                        dispatch(authActions.logout());
-                        navigate('/login');
-                    }
-                    if (err.response.data.detail === 'Token is blacklisted') {
-                        dispatch(authActions.logout());
-                        alert(
-                            '다른 곳에서 해당 계정으로 로그인하였습니다. 다시 로그인해주세요!'
-                        );
-                        navigate('/login');
-                    }
-                });
-            init = true;
-        }
+        fetchTokenInHome();
     }, [dispatch]);
-
+    const fetchTokenInHome = async () => {
+        const { exp, token } = await handleExpired();
+        dispatch(
+            authActions.login({
+                token: token.data.access,
+                exp,
+            })
+        );
+        console.log('REFRESHED');
+    };
     const menu = [
         [diary, '식단일기', 'diary'],
         [analysis, '식이분석', 'analysis'],
