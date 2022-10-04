621cea161187d4199890666b6075877e2e33a25f
Parkjju <rudwns3927@gmail.com>
2022-10-04 15:41:41 +0900

    feat: 로그아웃 상태에 따라 헤더 여부 표기

--- frontend/src/components/atom/Header/Header.jsx
+++ frontend/src/components/atom/Header/Header.jsx
@@ -2,20 +2,22 @@ import { Helmet } from 'react-helmet';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { HeaderBox, Icon } from './Header.style';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../../../store/auth-slice';
 
 const Header = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const location = useLocation();
     const locationArray = location.pathname.split('/');
+    const isLoggedIn = window.sessionStorage.getItem('isLoggedIn');
 
     const [backActive, setBackActive] = useState(true);
     const [homeActive, setHomeActive] = useState(true);
 
     useEffect(() => {
-        if (
-            location.pathname === '/login' ||
-            location.pathname === '/register'
-        ) {
+        if (isLoggedIn === 'false') {
             setBackActive(false);
             setHomeActive(false);
         } else {
@@ -33,68 +35,86 @@ const Header = () => {
                 />
             </Helmet>
 
-            <div
-                style={{
-                    paddingLeft: 30,
-                    width: 80,
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                }}
-            >
-                <Icon
-                    onClick={() => {
-                        if (!backActive) return;
-                        if (
-                            locationArray.length > 1 &&
-                            locationArray.length < 4
-                        ) {
-                            navigate('/');
-                            return;
-                        }
-                        if (locationArray[1].length > 0) {
-                            navigate(-1);
-                            return;
-                        }
-                        if (backActive) {
-                            navigate(-1);
-                        } else {
-                            return null;
-                        }
-                    }}
-                    active={backActive}
-                    className='material-symbols-outlined'
-                >
-                    arrow_back
-                </Icon>
-                <Icon
-                    onClick={() => {
-                        if (!homeActive) return;
-                        if (homeActive) {
-                            navigate('/');
-                        } else {
-                            return null;
-                        }
-                    }}
-                    active={homeActive}
-                    className='material-symbols-outlined'
-                >
-                    home
-                </Icon>
-            </div>
-            <Icon
-                onClick={() => {
-                    if (homeActive) {
-                        navigate('/');
-                    } else {
-                        return null;
-                    }
-                }}
-                active={homeActive}
-                className='material-symbols-outlined'
-                style={{ paddingRight: 30 }}
-            >
-                logout
-            </Icon>
+            {isLoggedIn ? (
+                <>
+                    <div
+                        style={{
+                            paddingLeft: 30,
+                            width: 80,
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <Icon
+                            onClick={() => {
+                                if (!backActive) return;
+                                if (
+                                    locationArray.length > 1 &&
+                                    locationArray.length < 4
+                                ) {
+                                    navigate('/');
+                                    return;
+                                }
+                                if (locationArray[1].length > 0) {
+                                    navigate(-1);
+                                    return;
+                                }
+                                if (backActive) {
+                                    navigate(-1);
+                                } else {
+                                    return null;
+                                }
+                            }}
+                            active={backActive}
+                            className='material-symbols-outlined'
+                        >
+                            arrow_back
+                        </Icon>
+                        <Icon
+                            onClick={() => {
+                                if (!homeActive) return;
+                                if (homeActive) {
+                                    navigate('/');
+                                } else {
+                                    return null;
+                                }
+                            }}
+                            active={homeActive}
+                            className='material-symbols-outlined'
+                        >
+                            home
+                        </Icon>
+                    </div>
+
+                    <Icon
+                        onClick={async () => {
+                            if (homeActive) {
+                                try {
+                                    await axios.post(
+                                        'https://www.nuseum.site/api/v1/account/logout/'
+                                    );
+
+                                    dispatch(authActions.logout());
+                                    window.sessionStorage.removeItem(
+                                        'isLoggedIn'
+                                    );
+                                    alert('로그아웃 되었습니다!');
+                                    navigate('/login');
+                                } catch (error) {
+                                    console.log(error);
+                                }
+                            } else {
+                                return null;
+                            }
+                        }}
+                        active={homeActive}
+                        className='material-symbols-outlined'
+                        style={{ paddingRight: 30 }}
+                    >
+                        logout
+                    </Icon>
+                </>
+            ) : null}
         </HeaderBox>
     );
 };
