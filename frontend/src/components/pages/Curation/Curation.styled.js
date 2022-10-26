b8bfb174040975b91abcd443b6d90eb869130881
Parkjju <rudwns3927@gmail.com>
2022-10-26 13:57:12 +0900

    style: 큐레이션 컴포넌트 - Warning으로 피해야할 식품 분리

--- frontend/src/components/pages/Curation/Curation.styled.js
+++ frontend/src/components/pages/Curation/Curation.styled.js
@@ -1,51 +1,40 @@
 import styled from 'styled-components';
 
 export const CurationBox = styled.div`
-    border: 1px solid #eeeeee;
-    width: 80%;
-    height: 120px;
-    border-top-left-radius: 30px;
-    border-top-right-radius: 30px;
-    display: flex;
-    align-items: center;
-    flex-direction: row;
-    max-width: 430px;
-    margin-bottom: 15px;
+    width: 100%;
+    height: 200px;
 `;
 
 export const CurationType = styled.div`
     background-color: rgba(238, 238, 238, 0.5);
     box-shadow: rgba(0, 0, 0, 0.24) 0px 1px 5px;
-    height: 100%;
-    min-width: 96px;
+    width: 100%;
+    height: 70px;
     display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
     border-top-left-radius: 30px;
     border-top-right-radius: 30px;
-    /* margin-right: 60px; */
-    position: relative;
-    left: 0;
-    @media all and (min-width: 420px) {
-        margin-right: 30px;
-    }
 `;
 export const CurationTypeImage = styled.img`
     height: 40px;
     width: 40px;
-    margin-bottom: 10px;
+    margin-right: 10px;
 `;
 export const CurationTypeName = styled.span`
     font-size: 14px;
 `;
 
 export const CurationFood = styled.div`
-    width: auto;
-    height: 80px;
+    width: 100%;
+    padding-top: 15px;
+    padding-bottom: 15px;
     display: flex;
     align-items: center;
     flex-direction: column;
+    border: 1px solid #eeeeee;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
 `;
 
 export const CurationFoodTitle = styled.span`
@@ -55,15 +44,18 @@ export const CurationFoodTitle = styled.span`
     text-align: center;
 `;
 export const CurationFoodList = styled.div`
-    height: 60px;
-    min-width: 210px;
+    min-height: 60px;
+    width: 100%;
     display: grid;
-    grid-template-rows: repeat(2, 1fr);
     grid-template-columns: repeat(2, 1fr);
     justify-items: center;
+    align-items: flex-start;
     text-align: center;
     margin-top: 15px;
     font-size: 12px;
+    font-weight: 100;
+    grid-auto-rows: auto;
+    padding: 5px;
 `;
 
 export const CommentBox = styled.div`
@@ -73,7 +65,74 @@ export const CommentBox = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    background-color: #e3e3e3;
+    background-color: #e5e5e5;
     font-size: 14px;
     line-height: 2;
+    margin-top: 50px;
+    border-radius: 30px;
+`;
+
+export const WarningBox = styled.div`
+    width: 80%;
+    min-height: 110px;
+    max-width: 430px;
+    background-color: #f7f7f7;
+    margin: 0 auto;
+    border-radius: 30px;
+    padding: 20px 30px;
+    border: 1px solid #eeeeee;
+    box-sizing: border-box;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const WarningTitle = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+    align-items: center;
+`;
+
+export const WarningList = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-rows: repeat(2, auto);
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: center;
+    align-items: flex-start;
+    row-gap: 10px;
+`;
+export const WarningFood = styled.span`
+    font-size: 13px;
+    font-weight: 100;
+    line-height: 1.5;
+`;
+export const WarningMain = styled.span`
+    font-size: 13px;
+    line-height: 1.5;
+    white-space: pre-line;
+    text-align: center;
+    text-decoration: underline;
+    font-weight: 500;
+`;
+
+export const CurationDataWrapper = styled.div`
+    width: 80%;
+    max-width: 430px;
+    display: grid;
+    column-gap: 10px;
+    row-gap: 30px;
+    grid-template-columns: repeat(2, 1fr);
+    margin-bottom: 30px;
+`;
+
+export const Title = styled.p`
+    width: 100%;
+    font-size: 18px;
+    font-weight: 600;
+    text-align: center;
+    margin: 20px 0;
 `;
