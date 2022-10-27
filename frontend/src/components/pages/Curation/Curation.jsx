d91d6cd7fc2ff061ada42b45493b5b1dfdb758ba
Parkjju <rudwns3927@gmail.com>
2022-10-27 17:50:18 +0900

    feat: 추천데이터 AnimatePresence 적용, drag 및 onDragEnd로 요소 탐색 로직 추가, Slide 컴포넌트 분리 및 불러온 뒤 렌더링 각각 진행

--- frontend/src/components/pages/Curation/Curation.jsx
+++ frontend/src/components/pages/Curation/Curation.jsx
@@ -1,5 +1,5 @@
 import Container from '../../atom/Container';
-import { AnimatePresence } from 'framer-motion';
+
 import { Contents } from '../Home/styled';
 
 import axios from 'axios';
@@ -10,15 +10,16 @@ import { authActions } from '../../../store/auth-slice';
 import { useState } from 'react';
 import { Title } from './Curation.styled';
 import Slide from './components/Slide';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Curation = () => {
     const dispatch = useDispatch();
-    const [recommendId, setRecommendId] = useState(null);
+
     const token = useSelector((state) => state.auth.token);
-    const [recommendData, setRecommendData] = useState(null);
-    const [currentRecommendIndex, setCurrentRecommendIndex] = useState(-1);
 
     const [recommendList, setRecommendList] = useState([]);
+    const [visibleIndex, setVisibleIndex] = useState(0);
+    const [isNegative, setIsNegative] = useState(true);
 
     const fetchId = async () => {
         try {
@@ -28,11 +29,9 @@ const Curation = () => {
                 },
             });
 
-            setRecommendId(response.data.at(currentRecommendIndex).id);
             setRecommendList(response.data);
         } catch (err) {
             console.log(err);
-            if (!recommendId) return;
             if (err.response?.status === 401) {
                 const { exp, token } = await handleExpired();
                 dispatch(
@@ -42,66 +41,65 @@ const Curation = () => {
                     })
                 );
             } else {
-                if (!recommendId) return;
                 alert('오류가 발생했습니다. 담당자에게 문의해주세요!');
             }
         }
     };
 
-    const fetchRecommend = async () => {
-        try {
-            const response = await axios.get(
-                `/api/v1/recommendation/user/${recommendId}/`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            );
-            setRecommendData(response.data);
-        } catch (err) {
-            console.log(err);
-            if (!recommendId) return;
-            if (err.response.status === 401) {
-                const { exp, token } = await handleExpired();
-                dispatch(
-                    authActions.login({
-                        token: token.data.access,
-                        exp,
-                    })
-                );
-            } else {
-                alert('오류가 발생했습니다. 담당자에게 문의해주세요!');
+    const setNextPage = (event, info) => {
+        if (info.offset.x < -200) {
+            if (visibleIndex === 0) {
+                return;
             }
+            setIsNegative(false);
+            setVisibleIndex((prev) => prev - 1);
+            console.log('prev!');
+        } else if (info.offset.x > 200) {
+            if (visibleIndex === recommendList.length - 1) {
+                return;
+            }
+            setIsNegative(true);
+            setVisibleIndex((prev) => prev + 1);
+            console.log('next!');
+        } else {
+            console.log('no changes!');
         }
     };
 
     useEffect(() => {
         fetchId();
     }, []);
-    useEffect(() => {
-        fetchRecommend();
-    }, [recommendId]);
-
-    useEffect(() => {
-        if (recommendList.length === 0) {
-            return;
-        }
-
-        setRecommendId(recommendList.at(currentRecommendIndex).id);
-    }, [currentRecommendIndex]);
-    console.log(recommendData);
 
     return (
         <Container>
             <Contents>
                 <AnimatePresence>
-                    <Slide
-                        setRecommendId={setRecommendId}
-                        recommendDataInfo={recommendList}
-                        recommendData={recommendData}
-                        setCurrentRecommendIndex={setCurrentRecommendIndex}
-                    />
+                    {recommendList.map((recommendData) =>
+                        recommendList.indexOf(recommendData) ===
+                        visibleIndex ? (
+                            <motion.div
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                }}
+                                drag='x'
+                                onDragEnd={setNextPage}
+                                key={recommendData.id}
+                                dragSnapToOrigin
+                                initial={{ x: isNegative ? -300 : 300 }}
+                                animate={{ x: 0 }}
+                                transition='none'
+                            >
+                                <Slide
+                                    date={recommendData.created_at}
+                                    id={recommendData.id}
+                                />
+                            </motion.div>
+                        ) : null
+                    )}
                 </AnimatePresence>
             </Contents>
         </Container>
