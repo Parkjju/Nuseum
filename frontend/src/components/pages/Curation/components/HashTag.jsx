f07bd3b4f3ad733611e86f0fb9b820e9bdf87b97
Parkjju <rudwns3927@gmail.com>
2022-11-02 18:07:47 +0900

    feat : 해시태그 컴포넌트 추가 및 모달창  슬라이딩 애니메이션 구현

---
+++ frontend/src/components/pages/Curation/components/HashTag.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    padding: 10px;
+    background: #e5e5e5;
+    border-radius: 20px;
+    width: auto;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 20px;
+    color: #2c2c2c;
+    margin-bottom: 10px;
+    cursor: pointer;
+    &:hover {
+        opacity: 0.8;
+    }
+`;
