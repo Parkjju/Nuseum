fea9b1dc60bbecec8843abda5d56115cd55a3cda
Parkjju <rudwns3927@gmail.com>
2022-11-02 18:05:17 +0900

    feat: 큐레이션 컴포넌트 슬라이딩 부여

--- frontend/src/components/pages/Curation/Curation.jsx
+++ frontend/src/components/pages/Curation/Curation.jsx
@@ -1,30 +1,24 @@
 import Container from '../../atom/Container';
+
 import { Contents } from '../Home/styled';
-import CurationData from './CurationData';
-import avoid from '../../../assets/curation/avoid.png';
+
 import axios from 'axios';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import handleExpired from '../../../helpers/handleExpired';
 import { authActions } from '../../../store/auth-slice';
 import { useState } from 'react';
-import {
-    CommentBox,
-    CurationDataWrapper,
-    CurationTypeImage,
-    Title,
-    WarningBox,
-    WarningFood,
-    WarningList,
-    WarningMain,
-    WarningTitle,
-} from './Curation.styled';
+import Slide from './components/Slide';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Curation = () => {
     const dispatch = useDispatch();
-    const [recommendId, setRecommendId] = useState(null);
+
     const token = useSelector((state) => state.auth.token);
-    const [recommendData, setRecommendData] = useState(null);
+
+    const [recommendList, setRecommendList] = useState([]);
+    const [visibleIndex, setVisibleIndex] = useState(0);
+    const [isNegative, setIsNegative] = useState(true);
 
     const fetchId = async () => {
         try {
@@ -34,10 +28,9 @@ const Curation = () => {
                 },
             });
 
-            setRecommendId(response.data[0].id);
+            setRecommendList(response.data);
         } catch (err) {
             console.log(err);
-            if (!recommendId) return;
             if (err.response?.status === 401) {
                 const { exp, token } = await handleExpired();
                 dispatch(
@@ -47,90 +40,66 @@ const Curation = () => {
                     })
                 );
             } else {
-                if (!recommendId) return;
                 alert('오류가 발생했습니다. 담당자에게 문의해주세요!');
             }
         }
     };
 
-    const fetchRecommend = async () => {
-        try {
-            const response = await axios.get(
-                `/api/v1/recommendation/user/${recommendId}/`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            );
-            setRecommendData(response.data);
-        } catch (err) {
-            console.log(err);
-            if (!recommendId) return;
-            if (err.response.status === 401) {
-                const { exp, token } = await handleExpired();
-                dispatch(
-                    authActions.login({
-                        token: token.data.access,
-                        exp,
-                    })
-                );
-            } else {
-                alert('오류가 발생했습니다. 담당자에게 문의해주세요!');
+    const setNextPage = (event, info) => {
+        if (info.offset.x < -200) {
+            if (visibleIndex === 0) {
+                return;
             }
+            setIsNegative(false);
+            setVisibleIndex((prev) => prev - 1);
+            console.log('prev!');
+        } else if (info.offset.x > 200) {
+            if (visibleIndex === recommendList.length - 1) {
+                return;
+            }
+            setIsNegative(true);
+            setVisibleIndex((prev) => prev + 1);
+            console.log('next!');
+        } else {
+            console.log('no changes!');
         }
     };
 
     useEffect(() => {
         fetchId();
     }, []);
-    useEffect(() => {
-        fetchRecommend();
-    }, [recommendId]);
+
     return (
         <Container>
             <Contents>
-                <Title>피해야 할 식품</Title>
-                <WarningBox>
-                    <WarningTitle>
-                        <CurationTypeImage src={avoid} alt='피해야할 음식' />
-                        <WarningMain>
-                            {
-                                recommendData?.data
-                                    .filter((item) => item.type === '주의')[0]
-                                    .main.split('(')[0]
-                            }
-
-                            {recommendData?.data
-                                .filter((item) => item.type === '주의')[0]
-                                .main.split('(')[1]
-                                ? `\n(${
-                                      recommendData?.data
-                                          .filter(
-                                              (item) => item.type === '주의'
-                                          )[0]
-                                          .main.split('(')[1]
-                                  }`
-                                : null}
-                        </WarningMain>
-                    </WarningTitle>
-                    <WarningList>
-                        {recommendData?.data
-                            .filter((item) => item.type === '주의')[0]
-                            .list.map((food, index) => (
-                                <WarningFood key={index}>{food}</WarningFood>
-                            ))}
-                    </WarningList>
-                </WarningBox>
-
-                <Title>내 아이 맞춤식품</Title>
-                <CurationDataWrapper rows={recommendData?.data.length / 2}>
-                    {recommendData?.data.map((item, index) => (
-                        <CurationData data={item} key={index} />
-                    ))}
-                </CurationDataWrapper>
-
-                <CommentBox>{recommendData?.comment}</CommentBox>
+                <AnimatePresence>
+                    {recommendList.map((recommendData) =>
+                        recommendList.indexOf(recommendData) ===
+                        visibleIndex ? (
+                            <motion.div
+                                style={{
+                                    width: '100%',
+                                    height: 'auto',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                }}
+                                drag='x'
+                                onDragEnd={setNextPage}
+                                key={recommendData.id}
+                                dragSnapToOrigin
+                                initial={{ x: isNegative ? -300 : 300 }}
+                                animate={{ x: 0 }}
+                                transition='none'
+                            >
+                                <Slide
+                                    date={recommendData.created_at}
+                                    id={recommendData.id}
+                                />
+                            </motion.div>
+                        ) : null
+                    )}
+                </AnimatePresence>
             </Contents>
         </Container>
     );
