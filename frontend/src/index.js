fe09ac9c657ded3b0d224607ba08c64ea0acc615
Parkjju <rudwns3927@gmail.com>
2023-03-10 17:03:24 +0900

    feat: QueryClientProvider 세팅

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,21 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import { RecoilRoot } from 'recoil';
 import axios from 'axios';
 import store from './store/store';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'https://www.nuseum.site/';
 // axios.defaults.baseURL = 'http://localhost:8000/';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RecoilRoot>
-        <Provider store={store}>
+    <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
             <App />
-        </Provider>
-    </RecoilRoot>
+        </QueryClientProvider>
+    </Provider>
 );
 
 serviceWorkerRegistration.register();
