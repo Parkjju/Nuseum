628a47676d95ba206aa19679bfa0c3f11ea0b702
Parkjju <rudwns3927@gmail.com>
2023-03-10 18:19:34 +0900

    feat: 영양제 / 물 / 오늘 탭 / 큐레이션리스트 / pdf fetching 로직 전체 외부 분리

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -11,3 +11,57 @@ export const fetchDailyFood = ({ queryKey }) => {
         }
     );
 };
+
+export const fetchSupplement = ({ queryKey }) => {
+    const [_, date, token] = queryKey;
+    return axios.get(
+        `https://www.nuseum.site/api/v1/consumption/supplement/?date=${date}`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }
+    );
+};
+
+export const fetchWaterAmount = ({ queryKey }) => {
+    const [_, date, token] = queryKey;
+    return axios.get(
+        `https://www.nuseum.site/api/v1/consumption/water/?date=${date}`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }
+    );
+};
+
+export const fetchTodayData = ({ queryKey }) => {
+    const [_, date, token] = queryKey;
+    return axios.get(
+        `https://www.nuseum.site/api/v1/consumption/today/?date=${date}`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }
+    );
+};
+
+export const fetchCurationList = ({ queryKey }) => {
+    const [_, token] = queryKey;
+    return axios.get(`https://www.nuseum.site/api/v1/recommendation/user/`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+};
+
+export const fetchPdf = ({ queryKey }) => {
+    const [_, token] = queryKey;
+    return axios.get(`https://www.nuseum.site/api/v1/result/examination/`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+};
