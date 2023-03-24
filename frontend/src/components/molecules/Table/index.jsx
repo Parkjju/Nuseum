e68cbe0c7143730a39d474e2ae0d1786357c4f00
Parkjju <rudwns3927@gmail.com>
2023-03-24 16:25:14 +0900

    feat: 테이블 컴포넌트 중복제거 로직 추가

--- frontend/src/components/molecules/Table/index.jsx
+++ frontend/src/components/molecules/Table/index.jsx
@@ -1,4 +1,45 @@
-const Table = ({ style }) => {
+import { useState } from 'react';
+import { useEffect } from 'react';
+import useCalculate from '../../../hooks/useCalculate';
+
+const Table = ({ nutritionData, curationData, style }) => {
+    console.log(nutritionData);
+    console.log(curationData);
+
+    // 3-5세 기준치
+    const baseline = useCalculate('M', 5);
+
+    // === 중복제거 리스트 및 가중치 초기화 ===
+    const [weight, setWeight] = useState({});
+    // const setData = (curationData) => {
+    //     let arr = [];
+
+    //     for (let obj of Object.values(curationData)) {
+    //         for (let data of Object.keys(obj)) {
+    //             arr.push(data);
+    //         }
+    //     }
+
+    //     // 추천음식리스트 중복제거
+    //     arr = [...Array.from(new Set(arr))];
+
+    //     for (let obj of arr) {
+    //         setWeight((prev) => {
+    //             prev[obj] = 0;
+
+    //             return { ...prev };
+    //         });
+    //     }
+    // };
+
+    // useEffect(() => {
+    //     setData(curationData);
+    // }, []);
+
+    // =====
+
+    const checkBaselineNutrition = () => {};
+
     return (
         <div
             style={{ ...style }}
@@ -10,32 +51,23 @@ const Table = ({ style }) => {
             <table>
                 <thead>
                     <tr>
-                        <th class='pin'> </th>
-                        <th>채소</th>
-                        <th>과일</th>
-                        <th>콩/두부</th>
-                        <th>통곡물</th>
-                        <th>해조류</th>
-                        <th>견과</th>
-                        <th>고기/어패류/달걀</th>
-                        <th>유제품</th>
+                        <th class='pin'></th>
+                        <th>DHA+EPA</th>
+                        <th>비타민 A</th>
+                        <th>마그네슘</th>
+                        <th>트립토판</th>
+                        <th>비타민 B6</th>
+                        <th>비타민 B12</th>
+                        <th>엽산</th>
+                        <th>비타민 D</th>
+                        <th>식이섬유</th>
                     </tr>
                 </thead>
 
                 <tbody>
                     <tr>
-                        <th>DHA와 EPA</th>
+                        <th>채소</th>
                         <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                    </tr>
-                    <tr>
-                        <th>비타민 A</th>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
@@ -46,18 +78,8 @@ const Table = ({ style }) => {
                         <td>Cell Data</td>
                     </tr>
                     <tr>
-                        <th>마그네슘</th>
+                        <th>과일</th>
                         <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                    </tr>
-                    <tr>
-                        <th>트립토판</th>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
@@ -68,18 +90,8 @@ const Table = ({ style }) => {
                         <td>Cell Data</td>
                     </tr>
                     <tr>
-                        <th>비타민 B6</th>
+                        <th>콩/두부</th>
                         <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                    </tr>
-                    <tr>
-                        <th>비타민 B6</th>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
@@ -90,18 +102,8 @@ const Table = ({ style }) => {
                         <td>Cell Data</td>
                     </tr>
                     <tr>
-                        <th>비타민 B12</th>
+                        <th>통곡물</th>
                         <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                        <td>Cell Data</td>
-                    </tr>
-                    <tr>
-                        <th>엽산</th>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
@@ -112,7 +114,8 @@ const Table = ({ style }) => {
                         <td>Cell Data</td>
                     </tr>
                     <tr>
-                        <th>비타민 D</th>
+                        <th>해조류</th>
+                        <td>Cell Data</td>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
@@ -123,7 +126,32 @@ const Table = ({ style }) => {
                         <td>Cell Data</td>
                     </tr>
                     <tr>
-                        <th>식이섬유</th>
+                        <th>견과</th>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                    </tr>
+                    <tr>
+                        <th>고기/어패류/달걀</th>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                        <td>Cell Data</td>
+                    </tr>
+                    <tr>
+                        <th>유제품</th>
+                        <td>Cell Data</td>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
                         <td>Cell Data</td>
