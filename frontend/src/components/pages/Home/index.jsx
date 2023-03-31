86ad5a90bf77716d1e6593ae9b10b4ab40f8b416
Parkjju <rudwns3927@gmail.com>
2023-03-31 14:39:55 +0900

    feat: 내 아이 메뉴 주석처리

--- frontend/src/components/pages/Home/index.jsx
+++ frontend/src/components/pages/Home/index.jsx
@@ -30,7 +30,7 @@ function Home() {
         [diary, lang ? 'DIARY' : '식단일기', 'diary'],
         [analysis, lang ? 'ANALYSIS' : '식이분석', 'analysis'],
         [food, lang ? 'FOOD' : '맞춤식품', 'food'],
-        [record, lang ? 'HEALTH RECORD' : '내 아이', 'record'],
+        // [record, lang ? 'HEALTH RECORD' : '내 아이', 'record'],
         [question, lang ? 'QUESTION' : 'Q&A', 'question'],
     ];
 
