a24488af00bd87e73871edf8f1a158a6b33a2155
ha-yun <deu05230naver.com>
2023-04-03 09:28:20 +0900

    style: home menu q&a수정

--- frontend/src/components/atom/Card/index.jsx
+++ frontend/src/components/atom/Card/index.jsx
@@ -32,7 +32,7 @@ function Card({ menu, current }) {
                                     }}
                                 >
                                     <IconBox>
-                                        <Icon src={item[0]} />
+                                        <Icon src={item[0]} alt='' />
                                         <IconName>{item[1]}</IconName>
                                     </IconBox>
                                 </Link>
@@ -69,6 +69,7 @@ function Card({ menu, current }) {
                                                     position: 'relative',
                                                 }}
                                                 src={item[0]}
+                                                alt=''
                                             />
                                             <IconName>{item[1]}</IconName>
                                         </IconBox>
@@ -89,7 +90,7 @@ function Card({ menu, current }) {
                                         }}
                                     >
                                         <IconBox>
-                                            <Icon src={item[0]} />
+                                            <Icon src={item[0]} alt=''/>
                                             <IconName>{item[1]}</IconName>
                                         </IconBox>
                                     </Link>
